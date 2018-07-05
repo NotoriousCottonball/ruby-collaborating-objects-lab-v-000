6e8e103d3f66b1eff1ff5550e79f6b5efd1d2ee2
George  Pianka <gpianka1@gmail.com>
2018-07-05 11:50:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
     @name = name 
   end 
   
-  def self.new_by_filename(file)
+ https://learn.co/tracks/full-stack-web-development-v5/object-oriented-ruby/object-relationships/collaborating-objects-lab# def self.new_by_filename(file)
     artist_name, song_name, genre = file.chomp(".mp3").split(/\s-\s/)
     self.new(song_name).artist = Artist.find_or_create_by_name(artist_name)
     
