cd3ec947f09d60aa59c113ab55342febc0bfc055
George  Pianka <gpianka1@gmail.com>
2018-07-05 11:21:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,6 @@ class Song
   def self.new_by_filename(file)
     artist_name, song_name, genre = file.chomp(".mp3").split(/\s-\s/)
     song = self.new(song_name)
-    song.artist = Artist.
+    song.artist = Artist.find
     
     
